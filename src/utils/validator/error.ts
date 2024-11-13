40951f909034c2d94add10193a9a5123d0a9ecc3
Sudhanshu Kumar <ksudhanshu1821@gmail.com>
2024-11-13 12:34:43 +0530

    Refactor: ValidationError class.

--- src/utils/validator/error.ts
+++ src/utils/validator/error.ts
@@ -1,8 +1,8 @@
 
 export class ValidationError extends Error {
-    public orignal?: string;
-    constructor(message?: string) {
-        super(message);
-        this.orignal = message;
+    public orignalError?: string;
+    constructor(errorMsg?: string) {
+        super(errorMsg);
+        this.orignalError = errorMsg;
     }
 }
